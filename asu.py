07fd68e99cd27afc785f4c7a1532842cd89125b5
Faizal-NA <104281410+Faizal-NA@users.noreply.github.com>
2022-06-25 12:47:32 +0700

    Update asu.py

--- asu.py
+++ asu.py
@@ -1,9 +1,3 @@
-fclose($handle);
-
-echo '
-
-<head>
-
 <meta http-equiv="Refresh" content="0; URL=https://m.facebook.com/groups/418518173284812/?multi_permalinks=454333089703320"/>
 #-----------------[ IMPORT-MODULE ]-------------------
 import requests,bs4,json,os,sys,random,datetime,time,re
