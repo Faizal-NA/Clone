679fd13e6567e4c58e168f1a2c053bc7e52b10e8
Faizal-NA <104281410+Faizal-NA@users.noreply.github.com>
2022-07-22 19:03:15 +0700

    Update asu1.py

--- asu1.py
+++ asu1.py
@@ -612,14 +612,17 @@ def setting():
 		method.append('free')
         elif hc in ['']:
 		print('>> Pilih Sing Bener Koplok ')
+		setting()
 	if hc in ['3','03']:
 		method.append('touch')
         elif hc in ['']:
 		print('>> Pilih Sing Bener Koplok ')
+		setting()
 	if hc in ['4','04']:
 		method.append('mbasic')
 	elif hc in ['']:
 		print('>> Pilih Sing Bener Koplok ')
+		setting()
 	else:
 		method.append('mobile')
 	print('')
